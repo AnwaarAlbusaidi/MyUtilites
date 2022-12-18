6d842c918d85575cd5a09df86bc4f4cc48a24549
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-18 00:18:55 -0800

    update in myFirstUtility

--- src/myFirstUtility.java
+++ src/myFirstUtility.java
@@ -7,17 +7,21 @@ public class myFirstUtility {
 	 */
 	public static void main(String[] args) {
 
-      int number [] = {5,6,10,1};
-      int maxNumber = 0;
-      
-      for(int i=0;i<number.length;i++)
-      {
-    	  if (number[i]> maxNumber )
-    		  maxNumber = number[i];
-      }
-      
-      System.out.println("This is the Max number " + maxNumber);
+      int number [] = {5,6,10,1,101};
+      int maxNumb = maxNumber(number);
+      System.out.println("This is the Max number " + maxNumb);
 
+	}
+	
+	public static int maxNumber(int [] intrgerArr)
+	{
+	      int maxNumber = 0;
+	      for(int i=0;i<intrgerArr.length;i++)
+	      {
+	    	  if (intrgerArr[i]> maxNumber )
+	    		  maxNumber = intrgerArr[i];
+	      }
+	      return maxNumber;
 	}
 
 }
