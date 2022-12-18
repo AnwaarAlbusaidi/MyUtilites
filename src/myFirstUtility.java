af274b8550d1f3941b8bba852c5fbbbd87e63c47
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-18 02:54:15 -0800

    updated

--- src/myFirstUtility.java
+++ src/myFirstUtility.java
@@ -5,20 +5,51 @@ public class myFirstUtility {
 	 */
 	public static void main(String[] args) {
 
-      int number [] = {5,6,10,1,101};
-      int maxNumb = maxNumber(number);
-      System.out.println("This is the Max number " + maxNumb);
-
+       int number [] = {50,12,8,10,1001,87,24};
+      //find the largest number
+      int maxNumb1 = maxNumber(number);
+      System.out.println("This is the Max number " + maxNumb1);
+      //find the largest number and second largest number
+      int [] maxNumb = new int [2];
+      maxNumb = maxNumber2(number);
+    	  System.out.print("[" + maxNumb[0] + "," + maxNumb[1]+ "]" );
 	}
+	
 	//Search from an array of integer and return the Max number
 	public static int maxNumber(int[] intrgerArr)
 	{
-	      int maxNumber = 0;
-	      for(int i=0;i<intrgerArr.length;i++)
+	      int maxNumber = intrgerArr[0];
+	      for(int i=0;i < intrgerArr.length;i++)
 	      {
-	    	  if (intrgerArr[i]> maxNumber )
-	    		  maxNumber = intrgerArr[i];
+	    		  if (intrgerArr[i] > maxNumber )
+	    		   maxNumber = intrgerArr[i];
 	      }
 	      return maxNumber;
 	}
-}//End of myFirstUtility
+	//Search from an array of integer for largest and 2nd largest number
+	public static int[] maxNumber2(int[] intrgerArr)
+	{
+	      int [] maxArray = new int[2];
+	      int max = intrgerArr[0];
+	      int max2 = 0;
+	      for(int i= 0;i < intrgerArr.length;i++)
+	      {
+	    		  if (intrgerArr[i] > max )
+	    		  {
+	    			   max = intrgerArr[i];
+	    		  }
+	      }
+	      maxArray[0] = max;
+	      
+	      for(int i= 0;i < intrgerArr.length;i++)
+	      {
+					if(intrgerArr[i] < maxArray[0] && intrgerArr[i] != maxArray[0] )
+					{
+						if (intrgerArr[i] > max2)
+							max2 = intrgerArr[i] ;
+					}	
+	      }
+	      maxArray[1] = max2;
+	      return maxArray;
+	}
+}//End of myFirstUtility 
