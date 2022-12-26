a76080656cf3b4b24beed47504cbf6d7d1c8e34a
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-25 21:46:44 -0800

    recursion class  updated

--- src/recursion.java
+++ src/recursion.java
@@ -30,21 +30,38 @@ public class recursion {
 //	    isPalindrome(example , 0);
 
 	}// End of main method
-
+    
+	/**
+	 * This method generate the power number for a given number
+	 * @param base
+	 * @param exponent
+	 * Input = 2,3 // output is 8
+	 * @return call to the same function to calculate the result
+	 */
 	public static int powerMethodRecursion(int base, int exponent) {
 		if (exponent == 0)
 			return 1;
 		else
 			return base * powerMethodRecursion(base, exponent - 1);
 	}
-
+    /**
+     * This method calculate the factorial for specific number
+     * @param number
+     * Input 5 // output 120
+     * @return call to the same function to generate the second number in the factorial series
+     */
 	public static int factorialMethodRecursion(int number) {
 		if (number == 1)
 			return 1;
 		else
 			return number * factorialMethodRecursion(number - 1);
 	}
-
+	/**
+	 * This method will reveres a given string and print it 
+	 * @param word
+	 * @param length
+	 * Input anwaar // output raawna
+	 */
 	public static void reverse(String word, int length) {
 		if (length < 0)
 			System.out.print("");
@@ -53,7 +70,13 @@ public class recursion {
 			reverse(word, length - 1);
 		}
 	}
-
+	/**
+	 * This method generate a series of increment and decrement for a given number
+	 * @param num
+	 * @param count
+	 * Input 4 // output  0 1 2 3 3 2 1 0
+	 * Input 5 // output  0 1 2 3 4 4 3 2 1 0
+	 */
 	public static void number(int num, int count) {
 		if (num == count)
 			return;
@@ -63,7 +86,13 @@ public class recursion {
 			System.out.print(" " + (num));
 		}
 	}
-
+	/**
+	 * This method generate a series of Fabonacci for a given number 
+	 * @param n
+	 * Input 8 /output is  0 1 1 2 3 5 8 13
+     * Input 9 / output is 0 1 1 2 3 5 8 13 21
+	 * @return call to the same function to generate the second number in the Fabonacci series
+	 */
 	public static int Fabonacci(int n) {
 		if (n == 0)
 			return 0;
@@ -72,7 +101,14 @@ public class recursion {
 		else
 			return Fabonacci(n - 1) + Fabonacci(n - 2);
 	}
-
+    
+	/**
+	  * This method will take a string and check if the given string is palindrome or not using recursion
+	  * @param word
+	  * Input madam // output true
+	  * Input something // output false
+	  * @return true if the string is palindrome and false if the string is not palindrome
+	  */
 	public static boolean isPalindrome() {
 		return true;
 	}
